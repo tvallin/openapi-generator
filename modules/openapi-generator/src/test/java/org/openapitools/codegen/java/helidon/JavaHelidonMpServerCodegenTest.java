6d77290a1a9cfc1b0a95b5223708c57e82edca55
Thibault Vallin <thibault.vallin@oracle.com>
2022-08-09 16:21:27 +0200

    Trigger GitHub actions (#31)
    * trigger GitHub actions
    
    Signed-off-by: tvallin <thibault.vallin@oracle.com>

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/java/helidon/JavaHelidonMpServerCodegenTest.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/java/helidon/JavaHelidonMpServerCodegenTest.java
@@ -44,181 +44,66 @@ public class JavaHelidonMpServerCodegenTest {
     }
 
     @Test
-    public void doGeneratePathParams() {
+    public void validatePetApi() {
         generate();
 
         JavaFileAssert.assertThat(Paths.get(apiPackage + "/PetService.java"))
-                .fileContains("import java.util.Objects;")
-                .assertMethod("deletePet", "ServerRequest", "ServerResponse")
-                .bodyContainsLines(
-                        "Long petId = Optional.ofNullable(request.path().param(\"petId\")).map(Long::valueOf).orElse" +
-                                "(null);",
-                        "ValidatorUtils.checkNonNull(petId);"
-                )
+                .fileContains("org.openapitools.server.model.Pet")
+                .assertMethod("addPet", "Pet")
                 .toFileAssert()
-                .assertMethod("getPetById")
-                .bodyContainsLines(
-                        "Long petId = Optional.ofNullable(request.path().param(\"petId\")).map(Long::valueOf).orElse" +
-                                "(null);",
-                        "ValidatorUtils.checkNonNull(petId);"
-                );
+                .assertMethod("addPets", "String", "InputStream", "InputStream", "List<String>", "List<Long>", "Integer")
+                .toFileAssert()
+                .assertMethod("deletePet", "Long", "String", "Long", "String", "Integer", "List<Integer>", "List<String>")
+                .toFileAssert()
+                .assertMethod("findPetsByStatus", "List<String>")
+                .toFileAssert()
+                .assertMethod("findPetsByTags", "List<Integer>")
+                .toFileAssert()
+                .assertMethod("getPetById", "Long")
+                .toFileAssert()
+                .assertMethod("updatePet", "Pet")
+                .toFileAssert()
+                .assertMethod("updatePetWithForm", "Long", "String", "String")
+                .toFileAssert()
+                .assertMethod("uploadFile", "Long", "Long", "String", "InputStream");
     }
 
     @Test
-    public void doGenerateQueryParams() {
+    public void validateStoreApi() {
         generate();
 
-        JavaFileAssert.assertThat(Paths.get(apiPackage + "/PetService.java"))
-                .fileContains("import java.util.List;")
-                .assertMethod("findPetsByTags")
-                .bodyContainsLines(
-                        "List<String> tags = Optional.ofNullable(request.queryParams().toMap().get(\"tags\"))" +
-                                ".orElse(null);",
-                        "ValidatorUtils.checkNonNull(tags);"
-                )
+        JavaFileAssert.assertThat(Paths.get(apiPackage + "/StoreService.java"))
+                .fileContains("org.openapitools.server.model.Order")
+                .assertMethod("deleteOrder", "String")
                 .toFileAssert()
-                .assertMethod("findPetsByStatus")
-                .bodyContainsLines(
-                        "List<String> status = Optional.ofNullable(request.queryParams().toMap().get(\"status\")).orElse" +
-                                "(null);",
-                        "ValidatorUtils.checkNonNull(status);"
-                );
+                .assertMethod("getInventory")
+                .toFileAssert()
+                .assertMethod("getOrderById", "BigDecimal")
+                .toFileAssert()
+                .assertMethod("placeOrder", "Order");
     }
 
     @Test
-    public void doGenerateBodyParams() {
+    public void validateUserApi() {
         generate();
-
-        JavaFileAssert.assertThat(Paths.get(apiPackage + "/PetService.java"))
-                .assertMethod("update")
-                .bodyContainsLines(
-                        "rules.post(\"/pet\", Handler.create(Pet.class, this::addPet));",
-                        "rules.put(\"/pet\", Handler.create(Pet.class, this::updatePet));"
-                )
-                .toFileAssert()
-                .assertMethod("addPet", "ServerRequest", "ServerResponse", "Pet")
-                .bodyContainsLines(
-                        "ValidatorUtils.checkNonNull(pet);",
-                        "handleAddPet(request, response, pet);"
-                )
-                .toFileAssert()
-                .assertMethod("updatePet", "ServerRequest", "ServerResponse", "Pet")
-                .bodyContainsLines(
-                        "ValidatorUtils.checkNonNull(pet);",
-                        "handleUpdatePet(request, response, pet);"
-                );
 
         JavaFileAssert.assertThat(Paths.get(apiPackage + "/UserService.java"))
-                .assertMethod("update")
-                .bodyContainsLines(
-                        "rules.post(\"/user\", Handler.create(User.class, this::createUser));",
-                        "rules.post(\"/user/createWithArray\", this::createUsersWithArrayInput);",
-                        "rules.post(\"/user/createWithList\", this::createUsersWithListInput);",
-                        "rules.put(\"/user/{username}\", Handler.create(User.class, this::updateUser));"
-                )
+                .fileContains("org.openapitools.server.model.User")
+                .assertMethod("createUser", "User")
                 .toFileAssert()
-                .assertMethod("createUser", "ServerRequest", "ServerResponse", "User")
-                .bodyContainsLines(
-                        "ValidatorUtils.checkNonNull(user);",
-                        "handleCreateUser(request, response, user);"
-                )
+                .assertMethod("createUsersWithArrayInput", "List<User>")
                 .toFileAssert()
-                .assertMethod("createUsersWithArrayInput", "ServerRequest", "ServerResponse")
-                .bodyContainsLines(
-                        "Single.create(request.content().as(new GenericType<List<User>>() { }))",
-                        ".thenAccept(user -> {",
-                        "ValidatorUtils.checkNonNull(user);",
-                        "handleCreateUsersWithArrayInput(request, response, user);",
-                        ".exceptionally(throwable -> handleError(request, response, throwable));"
-                );
-    }
-
-    @Test
-    public void doGenerateHeaderParams() {
-        generate();
-
-        JavaFileAssert.assertThat(Paths.get(apiPackage + "/PetService.java"))
-                .assertMethod("deletePet", "ServerRequest", "ServerResponse")
-                .bodyContainsLines(
-                        "String apiKey = request.headers().value(\"api_key\").orElse(null);",
-                        "Long headerLong = request.headers().value(\"headerLong\").map(Long::valueOf).orElse(null);",
-                        "ValidatorUtils.checkNonNull(headerLong);"
-                );
-    }
-
-    @Test
-    public void doGenerateCookiesParams() {
-        generate();
-
-        JavaFileAssert.assertThat(Paths.get(apiPackage + "/PetService.java"))
-                .assertMethod("deletePet", "ServerRequest", "ServerResponse")
-                .bodyContainsLines(
-                        "String cookieString = request.headers().cookies().toMap().getOrDefault(\"cookieString\", List.of" +
-                                "()).stream().findFirst().orElse(null);",
-                        "ValidatorUtils.checkNonNull(cookieString);",
-                        "Integer cookieInt = request.headers().cookies().toMap().getOrDefault(\"cookieInt\", List.of())" +
-                                ".stream().findFirst().map(Integer::valueOf).orElse(null);",
-                        "List<String> cookieIntArray = Optional.ofNullable(request.headers().cookies().toMap().get" +
-                                "(\"cookieIntArray\")).orElse(null);",
-                        "List<String> cookieStringArray = Optional.ofNullable(request.headers().cookies().toMap().get" +
-                                "(\"cookieStringArray\")).orElse(null);"
-                );
-    }
-
-    @Test
-    public void doGenerateFormParams() {
-        generate();
-
-        JavaFileAssert.assertThat(Paths.get(apiPackage + "/PetService.java"))
-                .assertMethod("addPets", "ServerRequest", "ServerResponse")
-                .bodyContainsLines(
-                        "Map<String, List<String>> nonFileFormContent = new HashMap<>();",
-                        "Map<String, List<InputStream>> fileFormContent = new HashMap<>();",
-                        " Single<Void> formSingle = request.content().asStream(ReadableBodyPart.class)",
-                        "if (\"images[]\".equals(name)) {",
-                        "processFileFormField(name, fileFormContent, part);",
-                        "if (\"image\".equals(name)) {",
-                        "if (\"titles[]\".equals(name)) {",
-                        "processNonFileFormField(name, nonFileFormContent, part);",
-                        "if (\"longArray\".equals(name)) {",
-                        "if (\"stringParam\".equals(name)) {",
-                        "if (\"intParam\".equals(name)) {",
-                        "List<InputStream> images = Optional.ofNullable(fileFormContent.get(\"images[]\")).orElse(null);",
-                        "InputStream image = Optional.ofNullable(fileFormContent.get(\"image\")).flatMap(list->list" +
-                                ".stream().findFirst()).orElse(null);",
-                        "List<String> titles = Optional.ofNullable(nonFileFormContent.get(\"titles[]\")).orElse(null);",
-                        "List<String> longArray = Optional.ofNullable(nonFileFormContent.get(\"longArray\")).orElse(null);",
-                        "Integer intParam = Optional.ofNullable(nonFileFormContent.get(\"intParam\")).flatMap(list->list" +
-                                ".stream().findFirst()).map(Integer::valueOf).orElse(null);"
-                );
-    }
-
-    @Test
-    public void doGenerateParamsValidation() {
-        generate();
-
-        JavaFileAssert.assertThat(Paths.get(apiPackage + "/PetService.java"))
-                .assertMethod("findPetsByStatus")
-                .bodyContainsLines(
-                        "ValidatorUtils.checkNonNull(status);",
-                        "List<String> status = Optional.ofNullable(request.queryParams().toMap().get(\"status\")).orElse" +
-                                "(null);"
-                )
+                .assertMethod("createUsersWithListInput", "List<User>")
                 .toFileAssert()
-                .assertMethod("findPetsByTags")
-                .bodyContainsLines(
-                        "List<String> tags = Optional.ofNullable(request.queryParams().toMap().get(\"tags\")).orElse" +
-                                "(null);",
-                        "ValidatorUtils.checkNonNull(tags);"
-                );
-
-        JavaFileAssert.assertThat(Paths.get(apiPackage + "/UserService.java"))
-                .assertMethod("loginUser")
-                .bodyContainsLines(
-                        "ValidatorUtils.validatePattern(username, \"^[a-zA-Z0-9]+[a-zA-Z0-9\\\\" +
-                                ".\\\\-_]*[a-zA-Z0-9]+$\");",
-                        ""
-                );
+                .assertMethod("deleteUser", "String")
+                .toFileAssert()
+                .assertMethod("getUserByName", "String")
+                .toFileAssert()
+                .assertMethod("loginUser", "String", "String", "String", "Long", "BigDecimal")
+                .toFileAssert()
+                .assertMethod("logoutUser")
+                .toFileAssert()
+                .assertMethod("updateUser", "String", "User");
     }
 
 }
